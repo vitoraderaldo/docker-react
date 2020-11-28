612613cf70559ff134ad38aba7cc28fbbe807c43
vitoraderaldo <60303480+vitoraderaldo@users.noreply.github.com>
2020-11-28 18:00:25 -0300

    change home (#1)
    Co-authored-by: Vitor <vitor_aderaldo@trimble.com>

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Running on container
+          Running a container on EC2 (Using ElasticBeanstalk) 
         </p>
         <a
           className="App-link"
